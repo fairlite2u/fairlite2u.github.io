cf3ece74621c1d34adb22bbac7a48b1c3da98026
fairlite2u <fairlite2u@gmail.com>
2019-05-31 22:32:11 -0600

    updatenow

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,8 +1,5 @@
-var now = new Date()
-var month = now.getMonth();
-var dayOfMonth = now.getDate();
-var dayOfWeek = now.getDay();
+var today = new Date()
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.write(day[today.getDay()] + ", " + today.getDate() + " " + month[today.getMonth()] + " " + today.getFullYear());
 
-
-
-document.getElementById('currentdate').in
