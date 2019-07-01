d1a64d08bc0f7547e91d85c420823004a9b1d491
fairlite2u <fairlite2u@gmail.com>
2019-06-30 20:52:02 -0600

    fixes

--- assignments/lesson-10/js/weatherdata.js
+++ assignments/lesson-10/js/weatherdata.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 let weatherRequest = new XMLHttpRequest();
 let apiURLstring1 = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=ff571c48af1923ac4fbdf7523b913171';
 weatherRequest.open('Get', apiURLstring1, true);
@@ -19,10 +26,6 @@ let apiURLstring2 = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473
 weatherTemps.open('Get', apiURLstring2, true);
 weatherTemps.send();
 
-let names = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-let d = new Date();
-let dayName = names[d.getDay()];
-
 weatherTemps.onload = function() {
     let weatherForecast = JSON.parse(weatherTemps.responseText);
     let tempforecast = [];
@@ -33,7 +36,6 @@ weatherTemps.onload = function() {
 
     console.log(weatherForecast);
 
-
     weatherForecast.list.forEach(hour => {
         if (hour.dt_txt.includes('18:00:00')) {
             tempforecast[day] = hour.main.temp;
@@ -43,11 +45,15 @@ weatherTemps.onload = function() {
         }
 
     });
+    let weekday = new Date();
+    let names = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 
     for (let i = 1; i <= 5; i++) {
         document.getElementById('forecast' + i).innerHTML = tempforecast[i].toFixed() + "&deg; F";
         document.getElementById('icon' + i).setAttribute('src', tempicon[i]);
         document.getElementById('icon' + i).setAttribute('alt', desc[i]);
+        document.getElementById('weekday' + i).innerHTML = names[(weekday.getDay() + i) % 7]; 
     }
     
 }
