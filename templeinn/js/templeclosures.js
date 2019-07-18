6f2a0a6072f59a919845329c7b14af2e8783a3e0
fairlite2u <fairlite2u@gmail.com>
2019-07-17 18:25:33 -0600

    temples

--- templeinn/js/templeclosures.js
+++ templeinn/js/templeclosures.js
@@ -3,23 +3,26 @@ let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
+
 request.onload = function() {
-    let towndata = request.response; 
-    let towns = towndata['towns'];
-    let output = document.querySelector('.eventbox');
-    for (let i = 0; i < towns.length; i++) {
-        if (towns[i].name == "Preston") {
-            let townevents = towns[i].events;
-            let numevents = 0; 
-            for (let e = 0; e < townevents.length; e++) {
-                let event = townevents[numevents];
-                numevents++;                 
+    let templedata = request.response; 
+    let temples = templedata['temples'];
+    let output = document.querySelector('.closurebox');
+    console.log(templedata);
+    for (let i = 0; i < temples.length; i++) {
+        if (temples[i].name == "Los Angeles California Temple") {
+            let templeclosures = temples[i].closures;
+            let numclosures = 0; 
+            for (let e = 0; e < templeclosures.length; e++) {
+                let closures = templeclosures[numclosures];
+                numclosures++;                 
                 let list = document.createElement('ul');
                 let listItem = document.createElement('li');
-                listItem.textContent = event;
+                listItem.textContent = closures;
                 list.appendChild(listItem);
                 output.appendChild(list);
             }
         }
     };
+
 }
